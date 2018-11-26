3b38d3bffbdb3fd6ff759a02f6d2d9d2d68c5b39
luciano2192 <lucianogreco92@gmail.com>
2018-11-26 14:08:06 -0300

    guia 1 finalizada, ya se ve todo el listado de peliculas

--- servidor/controladores/controlador.js
+++ servidor/controladores/controlador.js
@@ -1,8 +1,8 @@
-const conection = require('../lib/conexionbd');
+const connection = require('../lib/conexionbd');
 
 function showAllMovies(req,res) {
-    var showMoviesSql = 'select * from peliculas';
-    conection.query(showMoviesSql, function(error,results,fields) {
+    var showMoviesSql = 'select * from pelicula';
+    connection.query(showMoviesSql, function(error,results,fields) {
         if(error) {
             console.log('ERROR EN LA CONSULTA !', error.message);
             res.status(404).send('ERROR EN LA CONSULTA !');
--- servidor/lib/conexionbd.js
+++ servidor/lib/conexionbd.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
   port     : '3306',
   user     : 'root',
   password : 'admin',
-  database : 'queveo'
+  database : 'queVeo'
 });
 
 module.exports = connection;
